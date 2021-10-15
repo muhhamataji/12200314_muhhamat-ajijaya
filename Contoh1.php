a3eaf4e8008e292ad1c491ada3ced78cff7c2919
muhhamataji <ajiyjaya11@gmail.com>
2021-10-15 18:52:53 +0700

    upload1

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Muhhamat Ajijaya<br><hr>
+			 Saya tingga di bintaro<br><hr>
+			 olah raga yang saya sukai adalah
+ 			 renang dan futsal<hr>";
+	}
+}
